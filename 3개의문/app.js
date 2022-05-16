43323b95e6bbee10ad422c59ad563fa837d8e467
Eun-Gil Cho <76875723+USCgil0127@users.noreply.github.com>
2022-05-17 02:05:43 +0900

    DOM을 순회하는 횟수를 줄임

--- 3개의문/app.js
+++ 3개의문/app.js
@@ -1,8 +1,12 @@
 (function () {
   const container = document.querySelector(".container");
 
+  let Currentdoor;
+  let Currentimg;
+
   function container_control(e) {
-    remove_class();
+
+    remove_class(Currentdoor, Currentimg);
 
     const nearest_door = e.target.closest(".door");
     if (!nearest_door) return;
@@ -10,21 +14,25 @@
     if (!nearest_img) return;
 
     nearest_door.classList.add("door-open");
+    // 쿼리셀렉터를 쓰지않고, 열려있는 문을 잡아냄
+    Currentdoor = nearest_door;
 
     nearest_img.classList.add("img-open");
+    // 쿼리셀렉터를 쓰지않고, 잡아냄
+    Currentimg = nearest_img;
   }
 
   container.addEventListener("click", container_control);
 })();
 
-function remove_class() {
-  const Currentdoor = document.querySelector(".door-open");
+function remove_class(Currentdoor, Currentimg) {
+  // const Currentdoor = document.querySelector(".door-open");
 
   if (Currentdoor) {
     Currentdoor.classList.remove("door-open");
   }
 
-  const Currentimg = document.querySelector(".img-open");
+  // const Currentimg = document.querySelector(".img-open");
 
   if (Currentimg) {
     Currentimg.classList.remove("img-open");
