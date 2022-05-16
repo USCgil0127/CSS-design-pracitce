7525d4273847c8a85d7e0ef24b97d39344ef9ad6
Eun-Gil Cho <76875723+USCgil0127@users.noreply.github.com>
2022-05-17 01:46:25 +0900

    for문으로 작성된 remove_class() 리펙토링

--- 3개의문/app.js
+++ 3개의문/app.js
@@ -1,34 +1,33 @@
-const container = document.querySelector(".container");
+(function () {
+  const container = document.querySelector(".container");
 
-function container_control(e) {
-  remove_class();
+  function container_control(e) {
+    remove_class();
 
-  const nearest_door = e.target.closest(".door");
-  if (!nearest_door) return;
-  const nearest_img = e.target.nextElementSibling.firstElementChild;
-  if (!nearest_img) return;
+    const nearest_door = e.target.closest(".door");
+    if (!nearest_door) return;
+    const nearest_img = e.target.nextElementSibling.firstElementChild;
+    if (!nearest_img) return;
 
-  nearest_door.classList.add("door-open");
+    nearest_door.classList.add("door-open");
 
-  nearest_img.classList.add("img-open");
-}
+    nearest_img.classList.add("img-open");
+  }
 
-container.addEventListener("click", container_control);
+  container.addEventListener("click", container_control);
+})();
 
 function remove_class() {
-  const doorAll = document.querySelectorAll(".door");
-  for (let i = 0; i < doorAll.length; i++) {
-    const element = doorAll[i];
-    if (element.classList.contains("door-open")) {
-      element.classList.remove("door-open");
-    }
+  const Currentdoor = document.querySelector(".door-open");
+
+  if (Currentdoor) {
+    Currentdoor.classList.remove("door-open");
   }
 
-  const imgAll = document.querySelectorAll("img");
-  for (let i = 0; i < imgAll.length; i++) {
-    const element = imgAll[i];
-    if (element.classList.contains("img-open")) {
-      element.classList.remove("img-open");
-    }
+  const Currentimg = document.querySelector(".img-open");
+
+  if (Currentimg) {
+    Currentimg.classList.remove("img-open");
   }
 }
+
