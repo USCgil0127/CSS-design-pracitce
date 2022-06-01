914daaf4f0e13fb3f7b14a50e94fa4b5d8d46a56
Eun-Gil Cho <76875723+USCgil0127@users.noreply.github.com>
2022-06-02 08:42:56 +0900

    즉시 실행 익명함수로 코드 리펙토링
    전역 변수를 사용하지 않기 위해, 코드들을 즉시 실행 익명함수에 담아주었다.

--- 간단한 캐러셀/app.js
+++ 간단한 캐러셀/app.js
@@ -1,42 +1,44 @@
-const container = document.querySelector(".btn-container");
-let count = 1;
+(function () {
+  const container = document.querySelector(".btn-container");
+  let count = 1;
 
-container.addEventListener("click", clickAfter);
+  container.addEventListener("click", clickAfter);
 
-function clickAfter(e) {
-  const btn = e.target.closest("button");
+  function clickAfter(e) {
+    const btn = e.target.closest("button");
 
-  console.log(btn);
+    console.log(btn);
 
-  if (!btn) {
-    return;
-  }
+    if (!btn) {
+      return;
+    }
 
-  const imgContainer = document.querySelector(".slide-container");
+    const imgContainer = document.querySelector(".slide-container");
 
-  if (btn.classList.contains("slide-1")) {
-    imgContainer.style.transform = "translateX(0vw)";
-    count = 1;
-  } else if (btn.classList.contains("slide-2")) {
-    imgContainer.style.transform = "translateX(-100vw)";
-    count = 2;
-  } else if (btn.classList.contains("slide-3")) {
-    imgContainer.style.transform = "translateX(-200vw)";
-    count = 3;
-  }
-
-  if (btn.classList.contains("next")) {
-    if (count === 1) {
-      imgContainer.style.transform = "translateX(-100vw)";
-      count = 2;
-    } else if (count === 2) {
-      imgContainer.style.transform = "translateX(-200vw)";
-      count = 3;
-    } else if (count === 3) {
+    if (btn.classList.contains("slide-1")) {
       imgContainer.style.transform = "translateX(0vw)";
       count = 1;
+    } else if (btn.classList.contains("slide-2")) {
+      imgContainer.style.transform = "translateX(-100vw)";
+      count = 2;
+    } else if (btn.classList.contains("slide-3")) {
+      imgContainer.style.transform = "translateX(-200vw)";
+      count = 3;
     }
-  } else {
-    return;
+
+    if (btn.classList.contains("next")) {
+      if (count === 1) {
+        imgContainer.style.transform = "translateX(-100vw)";
+        count = 2;
+      } else if (count === 2) {
+        imgContainer.style.transform = "translateX(-200vw)";
+        count = 3;
+      } else if (count === 3) {
+        imgContainer.style.transform = "translateX(0vw)";
+        count = 1;
+      }
+    } else {
+      return;
+    }
   }
-}
+})();
